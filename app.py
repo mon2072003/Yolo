e6829d3cbd4d056ef33f0ac60fca506757a3487f
Mina Ezzat <136010819+mon2072003@users.noreply.github.com>
2025-04-24 02:15:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,6 @@ def predict():
             classes.append(cls)
     return jsonify({"classes": classes})
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
